69439e63418016bcf5b86ace9f46e9296c71da6f
Katya <kittyserenakat@gmail.com>
2023-02-02 15:57:06 -0500

    Add reducer helper function

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -82,24 +83,23 @@ export const CartProvider = ({ children }) => {
   const [ { cartItems, isCartOpen, cartCount, cartTotal }, dispatch ] = useReducer(cartReducer, INITIAL_STATE)
 
   const updateCartItemsReducer = (newCartItems) => {
-    const newCartCount = cartItems.reduce( 
+    const newCartCount = newCartItems.reduce( 
       ( total, cartItem ) =>  total + cartItem.quantity, 
       0
     );
 
-    const newCartTotal = cartItems.reduce( 
+    const newCartTotal = newCartItems.reduce( 
       ( total, cartItem ) => total + cartItem.quantity * cartItem.price, 
       0
     );
 
-    dispatch( {
-      type: CART_ACTION_TYPES.SET_CART_ITEMS, 
-      payload: { 
+    dispatch( 
+      createAction( CART_ACTION_TYPES.SET_CART_ITEMS, {
         cartItems: newCartItems, 
         cartTotal: newCartTotal, 
         cartCount: newCartCount,
-      },
-    });
+      })
+    )
   };
   
   
@@ -119,10 +119,7 @@ export const CartProvider = ({ children }) => {
   };
 
   const setIsCartOpen = (bool) => {
-    dispatch({ 
-      type: CART_ACTION_TYPES.SET_IS_CART_OPEN, 
-      payload: bool
-    });
+    dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool));
   }
 
   const value = { 
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useReducer } from 'react';
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 // STORAGE -- actual value you want to access
 export const UserContext = createContext({
@@ -35,7 +36,7 @@ export const UserProvider = ({ children }) => {
     const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE)
 
     const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
     }
 
     const value = { currentUser, setCurrentUser };
