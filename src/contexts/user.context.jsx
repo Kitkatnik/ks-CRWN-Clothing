03330aed4e82b4960529f95f173ff27d81ccfc41
Katya <kittyserenakat@gmail.com>
2023-02-03 16:21:54 -0500

    Add and set up Redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -13,6 +13,10 @@ export const USER_ACTION_TYPES = {
     'SET_CURRENT_USER': 'SET_CURRENT_USER'
 }
 
+const INITIAL_STATE = {
+    currentUser: null,
+}
+
 const userReducer = (state, action) => {
     const { type, payload } = action;
 
@@ -25,10 +29,6 @@ const userReducer = (state, action) => {
         default: 
             throw new Error(`Unhandled type ${type} in the userReducer`);
     }
-}
-
-const INITIAL_STATE = {
-    currentUser: null,
 }
 
 // COMPONENT -- actual functional component being used, with the value that holds what's in storage
--- src/index.js
+++ src/index.js
@@ -7,19 +7,24 @@ import { UserProvider } from './contexts/user.context'
 import { CategoriesProvider } from './contexts/categories.context'
 import { CartProvider } from './contexts/cart.context'
 
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
 import './index.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
